27756c12a0fd9c552b00383bec26893eba0780a3
DenisGeek <mail@denis.tk>
2019-04-20 22:55:31 +0200

    init MS.Task.Manager.DiscordTree2PlantUMLTree

---
+++ MS.Task.Manager.DiscordTree2PlantUMLTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MS.Task.Manager.DiscordTree2PlantUMLTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MS.Task.Manager.DiscordTree2PlantUMLTree");
+        }
+    }
+}
