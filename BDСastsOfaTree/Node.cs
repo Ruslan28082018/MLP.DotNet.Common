4b19cab364c8c75b0fedb0d19e559bc5573ded11
Руслан Ситдиков <legolaz2007@mail.ru>
2019-05-01 16:17:46 +0300

    FirstPush

---
+++ BDСastsOfaTree/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BDСastsOfaTree
+{
+    public class Node
+    {
+        public string Data;
+        public Node Parent;
+        public List<Node> Children = new List<Node>();
+    }
+}
---
+++ BDСastsOfaTree/PostgreSQLDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace BDСastsOfaTree
+{
+//класс контекста данных, на примере с сайта метанит
+    public class PostgreSQLDBContext: DbContext
+    {
+        public DbSet<Node> Nodes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=password");
+        }
+
+    }
+}
